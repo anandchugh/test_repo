699a58c2a0b5a7c6c1744887373d76be62d93ecf
anandchugh <3197424+anandchugh@users.noreply.github.com>
2022-02-13 18:29:14 +0530

    updaing test

--- test_main.py
+++ test_main.py
@@ -10,10 +10,6 @@ class Testing(unittest.TestCase):
         result = sum(1,2)
         self.assertEqual(result, 3)
 
-    
-    def test_3(self):
-        result = sum(1,2)
-        self.assertEqual(result, 5)
-        
+
 if __name__ == '__main__':
     unittest.main()
